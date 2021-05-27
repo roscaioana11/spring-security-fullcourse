2b4a07cca3c27fa09d3baf8249ae2034b5225fc4
Ioana Rosca <rosca.ioana11@gmail.com>
2021-05-27 19:57:26 +0300

    Setup

---
+++ src/main/java/com/example/springsecurityfullcourse/SpringSecurityFullcourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfullcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFullcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFullcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityfullcourse/SpringSecurityFullcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfullcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFullcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
